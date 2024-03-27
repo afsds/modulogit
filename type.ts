116f0f21ef61da5674d8149fefe064935108545e
afsds <142832572+afsds@users.noreply.github.com>
2024-03-27 18:53:47 -0300

    Add files via upload

---
+++ type.ts
@@ -0,0 +1,12 @@
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+
+console.log(multiplicacao(10, 6));
+console.log(saudacao("Caio Moreno"));
